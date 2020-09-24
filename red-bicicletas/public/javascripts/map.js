a04b43faa19991ce40b79f94caf6f64dc3ad695f
Nicolas <nicokenny98@gmail.com>
2020-09-24 15:06:25 -0300

    Pagina principal

---
+++ red-bicicletas/public/javascripts/map.js
@@ -0,0 +1,7 @@
+let map = L.map('main_map').setView([-34.636865, -58.372283],13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+}).addTo(map)
+
+L
